af28940e79151f18366d97269a721a0b16ace7bc
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-06-18 15:51:30 +0300

    dz 1 py task 6

--- PY_hw_1/hw_1.py
+++ PY_hw_1/hw_1.py
@@ -7,7 +7,7 @@ def ReadInt(args,long):
     while (len(entrence) != long ):
         entrence = input(f"invalid data, len =/= {long}, again: ")
         
-    return int(entrence)
+    return entrence
     
 
 # Задача 2: Найдите сумму цифр трехзначного числа.
@@ -17,7 +17,7 @@ def ReadInt(args,long):
 
 
 def task_2():
-    number = ReadInt("number task 2", 3)
+    number = int(ReadInt("number task 2", 3))
     sum = 0
     
     while (number >= 1):
@@ -52,4 +52,31 @@ def task_4():
     return f"Петя {x}, Катя {4 * x}, Сережа {x}"
     
 
-print(task_4())
+# print(task_4())
+
+
+
+# Задача 6: Вы пользуетесь общественным транспортом? 
+# Вероятно, вы расплачивались за проезд и получали билет с номером.
+# Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех. 
+# Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6.
+# Вам требуется написать программу, которая проверяет счастливость билета.
+# *Пример:*
+# 385916 -> yes
+# 123456 -> no
+
+
+def task_6():
+    bilet = ReadInt("bilet, task 6: ", 6)
+    bList = []
+    
+    for char in bilet:
+        bList.append(int(char))
+
+    if sum(bList[:len(bList)//2]) == sum(bList[3:len(bList)]):
+        return "yes"
+    else:
+        return "not lucky"
+
+
+print(task_6())
