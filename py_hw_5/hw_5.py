8311f896edd5adf279786dfe9ed94c9b0fed7a85
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-07-04 16:21:33 +0300

    dz 5 py task 26+28

--- py_hw_5/hw_5.py
+++ py_hw_5/hw_5.py
@@ -8,9 +8,34 @@
 def task26(a,b):  
     if b == 0:
         return 1
+    if b < 0:
+        return 1/a*task26(a, b+1)
     return a*task26(a, b-1)
     
-print(task26(int(input("A: ")), int(input("B: "))))
+print(task26(int(input("task 26 A: ")), int(input("task 26 B: "))))
 
 
 
+
+# Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. 
+# Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
+# *Пример:*
+# 2 2
+#     4
+
+
+def task28(a,b):
+    if a < 0:
+        a = int(input("A again: "))
+    if b < 0:
+        b = int(input("B again: "))
+    if a < b:
+        temp = a
+        a = b
+        b = temp
+    if b == 0:
+        return a
+    return task28(a+1,b-1)
+
+
+print(task28((int(input("task 28 A: "))), int(input("task 28 B: "))))
