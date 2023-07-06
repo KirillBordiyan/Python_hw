9562a765d4b10ddf880b848516afe6cd1aa55989
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-07-06 15:58:36 +0300

    dz 6 py task 32

--- py_hw_6/hw_6.py
+++ py_hw_6/hw_6.py
@@ -1,3 +1,4 @@
+import random
 # Задача 30:  Заполните массив элементами арифметической прогрессии.
 # Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
@@ -6,9 +7,9 @@
 
 def task30():
     
-    firstElemProgr = int(input("первый элемент: "))
-    scale = int(input("разность между элементами: "))
-    lenghtOfProgr = int(input("найти элемент: "))
+    firstElemProgr = int(input("task 30 первый элемент: "))
+    scale = int(input("task 30 разность между элементами: "))
+    lenghtOfProgr = int(input("task 30 найти элемент: "))
     arifm = [firstElemProgr]
     
     arifm = [firstElemProgr+((i-1)*scale) for i in range(1,lenghtOfProgr+1)]
@@ -16,3 +17,20 @@ def task30():
     return arifm
     
 print(task30())
+
+
+
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+def task32():
+    start = int(input("task 32 От: "))
+    end = int(input("task 32 До: "))
+    arrayOFNumbers = [random.randint(-100, 101) for _ in range(15)]
+    result = [i for i in arrayOFNumbers if start<i<end]
+    
+    print(arrayOFNumbers)
+    print(result)
+    
+task32()    
+    
