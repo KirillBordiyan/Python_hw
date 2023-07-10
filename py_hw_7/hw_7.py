dc74b3f75d2c9db81a0ee230d387a2bd901b48c7
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-07-10 09:13:52 +0300

    dz 7 py task 36

--- py_hw_7/hw_7.py
+++ py_hw_7/hw_7.py
@@ -34,4 +34,54 @@ def task34():
     else:
         print("Пам парам")
 
-task34()
+# task34()
+
+
+
+
+
+
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
+# Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
+# Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, 
+# например, у операции умножения.
+
+# *Пример:*
+
+# **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+# **Вывод:**
+
+# 1 2 3 4 5 6
+# 2 4 6 8 10 12
+# 3 6 9 12 15 18
+# 4 8 12 16 20 24
+# 5 10 15 20 25 30
+# 6 12 18 24 30 36
+
+
+def print_operation_table(oper, num_rows=6, num_columns=6):
+    
+    numbers = [[1]*num_columns for _ in range(num_rows)]
+   
+    for i in range(1):
+        for j in range(num_rows):
+            numbers[i][j] += j
+    
+    for i in range(1):
+        for j in range(num_columns):
+            numbers[j][i] += j
+    
+    for i in range(num_rows):
+        for j in range(num_columns):
+            numbers[i][j] = oper(numbers[i][0],numbers[0][j])
+   
+   
+    print(numbers)
+
+
+
+
+print_operation_table(oper = lambda x,y: x*y)
+
